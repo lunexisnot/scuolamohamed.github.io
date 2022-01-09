edd48370cebc190037001df7194232095701b475
lunexisnot <97397906+lunexisnot@users.noreply.github.com>
2022-01-09 19:50:25 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+let questions = [
+  {
+  numb: 1,
+  question: "un bambino su tre è..",
+  answer: "obeso",
+  options: [
+    "normale",
+    "in forma",
+    "un pochino in sovrappeso",
+    "sottopeso"
+  ]
+},
+  {
+  numb: 2,
+  question: "per cosa viene utilizzato il termine dieta??",
+  answer: "Per indicare il regime alimentare di una persona",
+  options: [
+    "Per indicare il regime alimentare di una persona",
+    "per indicare lo stato mentale di una persona",
+    "per indicare la quantià di cibo consumato da una persona",
+    "per indicare se una persona e in sovrappeso o in sottopeso",
+    
+  ]
+},
+  {
+  numb: 3,
+  question: "La dieta più seguita e la..",
+  answer: "Mediterranea",
+  options: [
+    "Mediterranea",
+    "Mind",
+    "Mayo Clini",
+    "Flexitaria"
+  ]
+},
+  {
+  numb: 4,
+  question: "in una dieta vegana deve essere presente..?",
+  answer: "Cereali",
+  options: [
+    "carne",
+    "Cereali",
+    "latte",
+    "acqua"
+  ]
+},
+  {
+  numb: 5,
+  question: "il fastfood e...",
+  answer: "un ristorante",
+  options: [
+    "un cibo",
+    "Un ristorante",
+    "un negozio",
+    "un gioco"
+  ]
+},
+,
+];
